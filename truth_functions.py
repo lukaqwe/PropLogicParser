68153e5b17d1ac7c10c7bd5557ce5b720e273042
lukaqwe <44036207+lukaqwe@users.noreply.github.com>
2019-04-16 10:44:30 +0300

    Update truth_functions.py

--- truth_functions.py
+++ truth_functions.py
@@ -1,13 +1,16 @@
 def neg(A):
-    C = [0]*len(A)
+    C = ['0']*len(A)
     for i in range(len(A)):
         if A[i] == '0':
             C[i] = '1'
     return C
 
 
-def impl(A, B):
-    return disj(neg(A), B)
+def disj(A, B):
+    C = []
+    for i in range(len(A)):
+        C.append(bin(int(A[i]) | int(B[i]))[-1])
+    return C
 
 
 def conj(A, B):
@@ -17,11 +20,9 @@ def conj(A, B):
     return C
 
 
-def disj(A, B):
-    C = []
-    for i in range(len(A)):
-        C.append(bin(int(A[i]) | int(B[i]))[-1])
-    return C
+def impl(A, B):
+    A = neg(A)
+    return disj(A, B)
 
 
 def equi(A, B):
