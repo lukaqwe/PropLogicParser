0d817cbed52513e8c3886d578713bc738e1b203e
lukaqwe <44036207+lukaqwe@users.noreply.github.com>
2019-04-16 10:43:58 +0300

    Update truth_table.py

--- truth_table.py
+++ truth_table.py
@@ -37,36 +37,6 @@ for i in range(len(L)):
             L[i] = truth_replace(j)
 
 
-def neg(A):
-    C = ['0']*len(A)
-    for i in range(len(A)):
-        if A[i] == '0':
-            C[i] = '1'
-    return C
-
-
-def disj(A, B):
-    C = []
-    for i in range(len(A)):
-        C.append(bin(int(A[i]) | int(B[i]))[-1])
-    return C
-
-
-def conj(A, B):
-    C = []
-    for i in range(len(A)):
-        C.append(bin(int(A[i]) & int(B[i]))[-1])
-    return C
-
-
-def impl(A, B):
-    A = neg(A)
-    return disj(A, B)
-
-
-def equi(A, B):
-    return conj(impl(A, B), impl(B, A))
-
 
 for i in L:
     for i in range(len(L)-1, -1, -1):
